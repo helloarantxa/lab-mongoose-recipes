7fd83f63d3d76b21effb0459d352d3e822904071
Arantxa Martinez <Shayne.martinez23@gmail.com>
2023-04-21 16:47:18 -0400

    lab done

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    let recipe = {
+      title: "cookies",
+      cuisine: "basic",
+    };
+    console.log("recipe title: " + recipe.title);
+    return Recipe.create(recipe);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+
+  .then(() => {
+    return Recipe.insertMany(data);
+  })
+
+  .then(() => {
+    Recipe.find().then((recipesFromDB) => {
+      recipesFromDB.forEach((recipe) => {
+        console.log(recipe.title);
+      });
+    });
+  })
+
+  .then(() => {
+    const query = { title: "Rigatoni alla Genovese" };
+    return Recipe.findOneAndUpdate(query, { duration: 100 });
+  })
+  .then(() => {
+    console.log("successfully updated duration");
+  })
+
+  .then(() => {
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then(() => {
+    console.log("sucessfully deleted Carrot Cake");
+    mongoose.connection.close();
+  })
+
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
